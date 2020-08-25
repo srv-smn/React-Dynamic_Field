c0465117c595bc3b1141150d60590891e4e324c6
sourav suman <saurav2fly@gmail.com>
2020-08-25 23:39:29 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
+import React,{Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Segment,Header, Card, Form, Container, Button, Dropdown } from 'semantic-ui-react' ;
 
-function App() {
+class App extends Component {
+  renderCards(){
+    const token = [
+  { key: 'dai', value: 'dai', text: 'Dai' },
+  { key: 'btc', value: 'btc', text: 'Bitcoin' },
+]
+  return(
+    <Segment inverted>
+    <Form inverted>
+      <Form.Group widths='equal'>
+        <Form.Input fluid  placeholder='Address' />
+        <Form.Input fluid  placeholder='Amount' />
+        <Dropdown
+    placeholder='Select Token'
+    fluid
+    search
+    selection
+    options={token}
+  />
+        <Button fluid type='submit'>+</Button>
+      </Form.Group>
+
+    </Form>
+
+    <Form inverted>
+      <Form.Group widths='equal'>
+        <Form.Input fluid  placeholder='Address' />
+        <Form.Input fluid  placeholder='Amount' />
+        <Dropdown
+    placeholder='Select Token'
+    fluid
+    search
+    selection
+    options={token}
+  />
+        <Button fluid type='submit'>+</Button>
+      </Form.Group>
+
+    </Form>
+
+    <Form inverted>
+      <Form.Group widths='equal'>
+        <Form.Input fluid  placeholder='Address' />
+        <Form.Input fluid  placeholder='Amount' />
+        <Dropdown
+    placeholder='Select Token'
+    fluid
+    search
+    selection
+    options={token}
+  />
+        <Button fluid type='submit'>+</Button>
+      </Form.Group>
+
+    </Form>
+  </Segment>
+  );
+}
+  HeaderExampleFloating(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <Segment>
+     <Header as ='h2' color = 'blue' textAlign = 'center'>
+     DevFolio ETHIndia
+     </Header>
+     </Segment>
+  );
+}
+  render(){
+  return (
+    <div>
+        {this.HeaderExampleFloating()}
+        <Container style = {{marginTop : '30px'}}>
+        {this.renderCards()}
+        </Container>
     </div>
   );
+}
 }
 
 export default App;
